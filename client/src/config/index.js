96fc8a9418d91883ee03692cd706f1ad83e7d753
Kartikey Keshari <kesharikartikey9140@gmail.com>
2025-03-17 23:55:31 +0530

    Added url

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -6,7 +6,7 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 config.NODE_ENV = process.env.NODE_ENV;
 config.VERSION = 'v1';
 
-config.API_END_POINT = isDevelopment ? 'http://localhost:5000/' : '/';
+config.API_END_POINT = isDevelopment ? 'https://snapfix.onrender.com/' : '/';
 
 config.GA_KEY = 'G-LBL46ZSPN5';
 
